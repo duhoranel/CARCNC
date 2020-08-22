81c5a6a8cb81f65cbbc6eca118b75deaff1a42d9
Laure Duhorane <laure.duhorane@lewagon.org>
2020-08-22 17:00:58 -0400

    routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
+  root to: 'cars#index'
+  resources :cars, only: [ :index, :new, :create, :show ]do
+    resources :bookings, only: [:new, :create]
+  end
+
+  patch '/bookings/:id', to:'bookings#update'
+  get '/bookings/owner', to: 'bookings#index_owner'
+  get '/bookings/renter', to: 'bookings#index_renter'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
