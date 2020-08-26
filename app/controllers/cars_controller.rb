3224c47c52ead55bc07ae1748b7ded76315b2c83
Cyrille Gawlowski <gawlowski.cyrille@gmail.com>
2020-08-25 21:35:40 -0400

    Cars MVC

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -2,4 +2,22 @@ class CarsController < ApplicationController
   def index
     @cars = Car.all
   end
+  def show
+    @car = Car.find(params[:id])
+  end
+  def def new
+    @car = Car.new
+  end
+  def create
+    @car = Car.new(car_params)
+    @car.save!
+    redirect_to root_path(@car)
+  end
+
+  private
+
+  def car_params
+    params.require(:car).permit(:model, :capacity, :availability, :price)
+  end
+
 end
