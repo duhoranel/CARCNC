353750f8208324c032457b0f1d784bb221d49d23
Laure Duhorane <laure.duhorane@lewagon.org>
2020-08-27 20:00:53 -0400

    bookings controllers & views

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,4 @@
 class Booking < ApplicationRecord
-  belongs_to :users
-  belongs_to :cars
+  belongs_to :user
+  belongs_to :car
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+new_car = Car.create!(model: 'test', price: '15', capacity: '6')
