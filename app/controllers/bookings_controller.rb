56f6aa29776c3fce5cdf5d9c42c9d68b10834b34
Laure Duhorane <laure.duhorane@lewagon.org>
2020-09-03 21:51:50 -0400

    book a car

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,50 +1,44 @@
 class BookingsController < ApplicationController
   def create
-    @booking = Booking.new(booking_params)
+    booking_params
+    @booking = Booking.new
+    @car = Car.find(params[:car_id])
+    @booking.car = @car
     @booking.user = current_user
-    @booking.car_id = params[:car_id]
-    if @booking.save!
-      redirect_to confirmation_path(@booking)
-    else
-      render :new
-    end
+      if @booking.save!
+        redirect_to car_booking_confirmation_path(@booking)
+      else
+        render "cars/show"
+      end
   end
-
   def update
-    if @booking.update(booking_params)
-      redirect_to confirmation_path
+    if @booking.update
+      redirect_to car_booking_confirmation_path(@booking)
     else
       render :new
     end
   end
-
   def dashboard
     @bookings = Booking.all
     @other_people_requests = Booking.joins(:cars).where(car: { user: current_user })
     @my_pending_requests = current_user.bookings
-
     # @bookings = cars_owned.map { |car| car.bookings }.flatten
   end
-
   def confirmation
     @booking = Booking.find(params[:booking_id])
   end
-
   def accept
     @booking = Booking.find(params[:booking_id])
     @booking.update(status: true)
-    redirect_to bookings_path
+    redirect_to root_path
   end
-
   def reject
     @booking = Booking.find(params[:booking_id])
     @booking.update(status: false)
-    redirect_to bookings_path
+    redirect_to root_path
   end
-
   private
-
   def booking_params
-    params.require(:booking).permit(:start_date, :end_date, :location)
+    params.permit(:car_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :cars, only: [ :index, :new, :create, :show ]do
-    resources :bookings, only: [:new, :create]
+    resources :bookings, only: [:new, :create] do
+      get "/confirmation", to: "bookings#confirmation"
+    end
   end
 
   patch '/bookings/:id', to:'bookings#update'
-  get "/confirmation", to: "bookings#confirmation"
+
   get '/bookings/dashboard', to: 'bookings#dashboard'
   patch "/accept", to: "bookings#accept", as: "accept"
   patch "/reject", to: "bookings#reject", as: "reject"
